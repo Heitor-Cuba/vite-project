814e794c28767c61835f5bb335b743eeff235dfc
Heitor-Cuba <heitorcuba@gmail.com>
2026-03-11 16:04:52 -0300

    Introdução aos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+interface ICardProps {
+  title: string;
+  children: React.ReactNode
+}
+
+const Card = (props: ICardProps) => {
+  return (
+    <div style={{ border: '1px solid black'}}>
+      <span> Title: {props.title} </span>
+
+      <div>
+        {props.children}
+      </div>
+
+      <div>
+        Footer
+      </div>
+    </div>
+  )
+}
+
 export function App() {
 
   return (
-    <>
+    <div>
       Olá
-    </>
+
+      <p> Card: </p>
+
+      <Card title='Teste'>
+        Teste
+      </Card>
+    </div>
   )
 }
