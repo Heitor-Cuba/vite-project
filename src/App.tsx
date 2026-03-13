a0c320690a004d9f77945a98d02b2d2f98dfe1f4
Heitor-Cuba <heitorcuba@gmail.com>
2026-03-13 01:12:33 -0300

    Utilizando key em listas

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react"
 
 export function App() {
-  const [count, setCount] = useState(0)
-  const [hide, setHide] = useState(false)
-
-  if (hide) return null;
+  const [list, setList] = useState([
+    {id: '1', label: 'Fazer café',},
+    {id: '2', label: 'Fazer almoço',},
+    {id: '3', label: 'Fazer janta',},
+  ]);
 
   return (
     <div>
-      {hide && <p> Teste 1 </p>}
-      {!hide && <p> Teste 2 </p>}
+      <input/>
 
-    {hide
-      ? <p> Teste 1 </p>
-      : <p> Teste 2 </p>
-    }
-
-      <button onClick={() => setCount(count + 1)}>
-        {count}
+      <button>
+        Adicionar
       </button>
 
-      <button onClick={() => setHide(!hide)}>
-        Toggle
-      </button>
-
+      <ol>
+        {list.map((listItem) => (
+          <li key={listItem.id}> 
+            {listItem.label}
+          </li>
+        ))}
+      </ol>
     </div>
   )
 }
